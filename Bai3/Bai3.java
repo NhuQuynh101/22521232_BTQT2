4db689d0f7a2e5c0d549ad9e2452576b2f25a3d5
NhuQuynh101 <163820289+NhuQuynh101@users.noreply.github.com>
2025-03-18 12:14:20 +0700

    commit

--- Bai3/Bai3.java
+++ Bai3/Bai3.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Scanner;
 
-public class Bai3a {
+public class Bai3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<Integer> arr = new ArrayList<>();
